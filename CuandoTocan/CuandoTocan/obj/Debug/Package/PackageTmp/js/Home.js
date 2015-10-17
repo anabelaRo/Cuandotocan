f61cac0677aea8b6262b3a1bb12b42a6ead922a7
Sobrile <juan_sobrile@hotmail.com>
2015-10-17 15:16:10 -0300

    Maquetado 2

--- CuandoTocan/CuandoTocan/obj/Debug/Package/PackageTmp/js/Home.js
+++ CuandoTocan/CuandoTocan/obj/Debug/Package/PackageTmp/js/Home.js
@@ -1,28 +1,41 @@
-imagenes = new Array();
-//Agrega tantas imágenes como necesites en el array
-imagenes[0] = "https://lh4.googleusercontent.com/-dN3EpFCaSLM/UHmHDm-BPyI/AAAAAAAACg0/Sg6_mUHUdKo/w719-h539-no/3.jpg";
-imagenes[1] = "https://lh4.googleusercontent.com/-uGLzLpo9M2I/UHi8QUuTaKI/AAAAAAAACXc/RDXnT81tRHc/w719-h539-no/3.jpg";
-imagenes[2] = "https://lh4.googleusercontent.com/-fiZRV54IhMg/UHsNjXjq1eI/AAAAAAAAC8g/d3vENTsqfRA/w809-h539-no/1.jpg";
-imagenes[3] = "https://lh4.googleusercontent.com/-31VDTWL4ZHc/UHhLG4xDA9I/AAAAAAAACIs/v48eQW0L8t0/w719-h539-no/1.jpg";
-imagenes[4] = "https://lh3.googleusercontent.com/-eL4wCSVNN2k/UHrjyxbgqKI/AAAAAAAAC1A/kLiHuBDhu54/w807-h539-no/1.jpg";
-imagenes[5] = "https://lh6.googleusercontent.com/-ey5kQS1mkXk/UGmw2GoAC3I/AAAAAAAABM4/KZijAmmhoKk/w718-h539-no/1.jpg";
-siguiente = 0;
-
-function CambiaImagen() {
-    dimensiones = $("#box-imagen").width();
-    $("#box-imagen img").css({ left: -dimensiones + "px" }).attr("src", imagenes[siguiente]);
-    $("#box-imagen img").animate({ left: "+=" + dimensiones });
-    $("#box-imagen a").attr("href", imagenes[siguiente]);
-
-    siguiente = siguiente + 1;
-    if (siguiente >= imagenes.length) {
-        siguiente = 0;
+//Slider:
+jssor_1_slider_init = function() {
+    var jssor_1_SlideshowTransitions = [
+        {$Duration:1200,$Opacity:2}
+    ];
+            
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $SlideshowOptions: {
+        $Class: $JssorSlideshowRunner$,
+        $Transitions: jssor_1_SlideshowTransitions,
+        $TransitionsOrder: 1
+        },
+        $ArrowNavigatorOptions: {
+        $Class: $JssorArrowNavigator$
+        },
+        $BulletNavigatorOptions: {
+        $Class: $JssorBulletNavigator$
+        }
+    };
+            
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales while window resizes
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 600);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
     }
-    //Aquí puedes colocar el tiempo para cada transición de imagen
-    transicion = 4000;
-    setTimeout("CambiaImagen()", transicion);
-}
-
-$(function () {
-    CambiaImagen();
-});
+    ScaleSlider();
+    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+    $Jssor$.$AddEvent(window, "resize", $Jssor$.$WindowResizeFilter(window, ScaleSlider));
+    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+    //responsive code end
+};
