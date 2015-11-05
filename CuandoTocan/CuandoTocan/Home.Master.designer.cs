d691de8ccd4c2b676dce93f441e18f415d23fc70
Anabela <anabela.rossi@gmail.com>
2015-11-05 13:27:18 -0300

    Tipo de busqueda

--- CuandoTocan/CuandoTocan/Home.Master.designer.cs
+++ CuandoTocan/CuandoTocan/Home.Master.designer.cs
@@ -40,13 +40,13 @@ namespace CuandoTocan {
         protected global::System.Web.UI.WebControls.Image imgLogo;
         
         /// <summary>
-        /// ddlTipoBusqueda control.
+        /// ddlTiBusqueda control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlTipoBusqueda;
+        protected global::System.Web.UI.WebControls.DropDownList ddlTiBusqueda;
         
         /// <summary>
         /// divLogin control.
